60968aba5a8051e84db29031a4cd7e68a29129c6
Brian <Brian@Brian-PC>
2017-01-25 18:00:14 -0700

    01/26/2017 Commit Mathcadia.java

--- Mathcadia/src/mathcadia/Mathcadia.java
+++ Mathcadia/src/mathcadia/Mathcadia.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
  */
 package mathcadia;
 
@@ -27,5 +25,6 @@ public class Mathcadia {
         String playerInfo = playerOne.toString();
         System.out.println(playerInfo);
     }
+
     
 }
