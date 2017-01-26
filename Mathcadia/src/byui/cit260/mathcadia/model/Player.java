d5c87c616ebcaee7cbf45795f7792ad128c2176f
Brian <Brian@Brian-PC>
2017-01-25 17:45:53 -0700

    01/24/2017 Initial Commit (to bottom page 8 of Team Assignmnet) 01/25/2017 Push changes

--- Mathcadia/src/byui/cit260/mathcadia/model/Player.java
+++ Mathcadia/src/byui/cit260/mathcadia/model/Player.java
@@ -6,11 +6,97 @@
 package byui.cit260.mathcadia.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
  * @author Brian
  */
 public class Player implements Serializable{
+    private String userName;
+    private String playerInventory;
+    private double experience;
+    private double bestScore;
+
+    public Player() {
+    }
+    
+    
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPlayerInventory() {
+        return playerInventory;
+    }
+
+    public void setPlayerInventory(String playerInventory) {
+        this.playerInventory = playerInventory;
+    }
+
+    public double getExperience() {
+        return experience;
+    }
+
+    public void setExperience(double experience) {
+        this.experience = experience;
+    }
+
+    public double getBestScore() {
+        return bestScore;
+    }
+
+    public void setBestScore(double bestScore) {
+        this.bestScore = bestScore;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 37 * hash + Objects.hashCode(this.userName);
+        hash = 37 * hash + Objects.hashCode(this.playerInventory);
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.experience) ^ (Double.doubleToLongBits(this.experience) >>> 32));
+        hash = 37 * hash + (int) (Double.doubleToLongBits(this.bestScore) ^ (Double.doubleToLongBits(this.bestScore) >>> 32));
+        return hash;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "userName=" + userName + ", playerInventory=" + playerInventory + ", experience=" + experience + ", bestScore=" + bestScore + '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (Double.doubleToLongBits(this.experience) != Double.doubleToLongBits(other.experience)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.bestScore) != Double.doubleToLongBits(other.bestScore)) {
+            return false;
+        }
+        if (!Objects.equals(this.userName, other.userName)) {
+            return false;
+        }
+        if (!Objects.equals(this.playerInventory, other.playerInventory)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     
 }
