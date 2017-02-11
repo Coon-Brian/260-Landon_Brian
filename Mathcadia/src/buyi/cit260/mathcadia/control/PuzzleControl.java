e386d218f02256f347531d9d22bb6f3503f593f4
landonrs <altruist8@yahoo.com>
2017-02-11 16:06:49 -0700

    Added code and test for isFactor() function

--- Mathcadia/src/buyi/cit260/mathcadia/control/PuzzleControl.java
+++ Mathcadia/src/buyi/cit260/mathcadia/control/PuzzleControl.java
@@ -46,5 +46,34 @@ public class PuzzleControl {
         
     }
     
+    /** public int isFactor(String number, String potentialFactor)
+    * Description: the user is asked to enter a number greater than 100 and then 
+    * a smaller number greater than 2 that is a factor of the first. They will 
+    * input two numbers. The function tests if the smaller number is a factor of
+    * the larger. If it is, the function returns the other factor.
+    * @param number
+    * @param potentialFactor 
+    */
+    public int isFactor(String largeNum, String potentialF){
+        //make sure input is valid
+        // if the string contains anything but int, return failed
+       if (!largeNum.matches("^\\d*$") || !potentialF.matches("^\\d*$"))
+            return  -1;
+       
+        //convert input to int
+        int number = Integer.parseInt(largeNum);
+        int potentialFactor = Integer.parseInt(potentialF);
+        
+        
+        if (number <= 100 || potentialFactor <= 2)
+            return -1;
+
+        //check if it is a factor
+        if (number % potentialFactor == 0)
+            return number / potentialFactor;
+        else 
+            return -1;
+
+    }
     
 }
--- Mathcadia/test/buyi/cit260/mathcadia/control/PuzzleControlTest.java
+++ Mathcadia/test/buyi/cit260/mathcadia/control/PuzzleControlTest.java
@@ -61,5 +61,57 @@ public class PuzzleControlTest {
         assertEquals(expResult, result);
         
     }
+
+    /**
+     * Test of isFactor method, of class PuzzleControl.
+     */
+    @Test
+    public void testIsFactor() {
+        System.out.println("isFactor");
+        
+        System.out.println("Test Case #1");
+        String number = "125";
+        String potentialFactor = "5";
+        PuzzleControl instance = new PuzzleControl();
+        int expResult = 25;
+        int result = instance.isFactor(number, potentialFactor);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case #2");
+        number = "85";
+        potentialFactor = "3";
+        expResult = -1;
+        result = instance.isFactor(number, potentialFactor);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case #3");
+        number = "v";
+        potentialFactor = "v";
+        expResult = -1;
+        result = instance.isFactor(number, potentialFactor);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case #4");
+        number = "104";
+        potentialFactor = "2";
+        expResult = -1;
+        result = instance.isFactor(number, potentialFactor);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case #5");
+        number = "102";
+        potentialFactor = "51";
+        expResult = 2;
+        result = instance.isFactor(number, potentialFactor);
+        assertEquals(expResult, result);
+        
+        System.out.println("Test Case #6");
+        number = "126";
+        potentialFactor = "3";
+        expResult = 42;
+        result = instance.isFactor(number, potentialFactor);
+        assertEquals(expResult, result);
+        
+    }
     
 }
